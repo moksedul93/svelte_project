64daac44bae24888f41b5fe2a68e47bdb6418971
Moksedul Islam <45994903+moksedul93@users.noreply.github.com>
2023-09-03 01:03:55 +0600

    added design

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,54 +1,47 @@
-<script>
-	import Counter from './Counter.svelte';
-	import welcome from '$lib/images/svelte-welcome.webp';
-	import welcome_fallback from '$lib/images/svelte-welcome.png';
-</script>
-
 <svelte:head>
 	<title>Home</title>
 	<meta name="description" content="Svelte demo app" />
 </svelte:head>
 
-<section>
-	<h1 class="text-3xl font-bold underline">
-  Hello world!
-</h1>
 
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
-
-	<Counter />
-</section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 0.6;
-	}
-
-	h1 {
-		width: 100%;
-	}
-
-	.welcome {
-		display: block;
-		position: relative;
-		width: 100%;
-		height: 0;
-		padding: 0 0 calc(100% * 495 / 2048) 0;
-	}
-
-	.welcome img {
-		position: absolute;
-		width: 100%;
-		height: 100%;
-		top: 0;
-		display: block;
-	}
-</style>
+<div class="layout">
+	<div class="md:flex">
+		<div class="w-full h-full md:w-6/12">
+			<div class="flex">
+				<div class="flex flex-col w-4/12">
+						<div class="w-full h-96 bg-blue-400 flex justify-center items-center">
+							<div>3 x 2(color:blue-400)</div>
+						</div>
+						<div class="w-full h-64 bg-teal-400 flex justify-center items-center">
+							<div>3 x 2(color:teal-400)</div>
+						</div>
+				</div>
+				<div class="w-8/12 bg-rose-100 flex justify-center items-center">
+					<div>5 x 4(color:rose-400)</div>
+				</div>
+			</div>
+			<div class="w-full h-32 bg-fuchsia-400 flex justify-center items-center">
+					<div>2 x 2(color:fuchsia-400)</div>
+			</div>
+		</div>
+		<div class="w-full h-full md:w-6/12">
+			<div class="w-full h-64 bg-blue-400 flex justify-center items-center">
+					<div>2 x 6(color:blue-400)</div>
+			</div>
+			<div class="flex">
+				<div class="w-8/12 h-[32rem] bg-yellow-100 flex flex-col justify-center items-center">
+					<div>4 x 4(color:yellow-100)</div>
+					<div class="h-[300px] w-[300px] bg-[#293462] flex justify-center items-center">
+							<div class="h-[200px] w-[200px] bg-yellow-100 rounded-full relative">
+								<div class="c3 w-[120px] h-[120px]"><div class="circ3"></div></div>
+								<div class="c4 w-[150px] h-[150px]"><div class="circ4"></div></div>
+							</div>
+					</div>
+				</div>
+				<div class="w-4/12 h-[32rem] bg-emerald-400 flex justify-center items-center">
+					<div>4 x 2(color:emerald-400)</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
