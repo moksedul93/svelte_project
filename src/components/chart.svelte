3b70584f6c969091d341e1a2123547ff54863499
Moksedul Islam <45994903+moksedul93@users.noreply.github.com>
2023-09-03 22:51:29 +0600

    remove console

--- src/components/chart.svelte
+++ src/components/chart.svelte
@@ -6,7 +6,6 @@
 		try {
 			const response = await fetch("https://restcountries.com/v3.1/all");
 			const data = await response.json();
-			console.log("Fetched data:", data); // Check if data is fetched
 			countryDataStore.set(data);
 		} catch (error) {
 			console.error("Error fetching data:", error);
